beb205a5329cad23c32b76f08e8a7334562bbb73
Eyy39 <phonsovaney8@gmail.com>
2026-03-05 16:08:53 +0700

    second

--- exercise-4/src/components/Carousel.jsx
+++ exercise-4/src/components/Carousel.jsx
@@ -9,26 +9,25 @@ export const Carousel = ({ images }) => {
   function onClickLeft(){
     if(cur>0){
       setCur(cur-1);
+    }else{
+      setCur(images.length-1);
     }
   }
   function onClickRight(){
     if(cur<images.length-1){
       setCur(cur+1);
+    }else{
+      setCur(0);
     }
   }
   /* You will need to manage the cases when we are on the last image or first image*/
-  function getClassName(index){
-    if(index===cur){
-      return ""
-    }
-  }
 
   return (
     <div className="carousel">
       <BsArrowLeftCircleFill className="arrow arrow-left" onClick={onClickLeft}/>
 
       {/* YOu will need to display the current image, not the first one.. */}
-      <img src={images[0].src} alt={images[0].alt} className="slide" />
+      <img src={images[cur].src} alt={images[cur].alt} className="slide" />
 
       <BsArrowRightCircleFill className="arrow arrow-right" onClick={onClickRight}/>
     </div>
